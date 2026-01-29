6c635f0b6d154aa5fe037e7dffa85959d9b34122
Mario Jimenez <marietejr12@gmail.com>
2026-01-29 19:04:18 +0100

    Cambio el puerto 5000 en la clase Cliente

--- ChattyTCP/Cliente.java
+++ ChattyTCP/Cliente.java
@@ -12,81 +12,69 @@ public class Cliente {
 
     public Cliente(Socket socket, String nombreCliente){
         try{
-            this.socket=socket;
-            this.nombreCliente=nombreCliente;
-            this.bufferedReader= new BufferedReader(
+            this.socket = socket;
+            this.nombreCliente = nombreCliente;
+            this.bufferedReader = new BufferedReader(
                     new InputStreamReader(socket.getInputStream()));
             this.bufferedWriter = new BufferedWriter(
                     new OutputStreamWriter(socket.getOutputStream()));
         } catch (IOException e){
-            e.printStackTrace();
-            cerrarComunicacion(socket, bufferedWriter, bufferedReader);
+            cerrarComunicacion();
         }
     }
 
-    public void cerrarComunicacion(Socket socket, BufferedWriter bufferedWriter, BufferedReader bufferedReader){
+    private void cerrarComunicacion(){
         try{
-            if (socket != null){
-                socket.close();
-            }
-            if (bufferedReader != null){
-                bufferedReader.close();
-            }
-            if (bufferedWriter != null){
-                bufferedWriter.close();
-            }
-        }catch (IOException e) {
+            if (socket != null) socket.close();
+            if (bufferedReader != null) bufferedReader.close();
+            if (bufferedWriter != null) bufferedWriter.close();
+        } catch (IOException e){
             e.printStackTrace();
         }
     }
 
     public void enviarMensaje(){
         try{
+            // Enviar nombre al servidor
             bufferedWriter.write(nombreCliente);
             bufferedWriter.newLine();
             bufferedWriter.flush();
+
             Scanner scanner = new Scanner(System.in);
+
             while (socket.isConnected()){
-                System.out.println("Escribe el mensaje: ");
-                String mensaje = scanner.next();
-                bufferedWriter.write(nombreCliente +": " + mensaje);
+                String mensaje = scanner.nextLine();
+                bufferedWriter.write(nombreCliente + ": " + mensaje);
                 bufferedWriter.newLine();
                 bufferedWriter.flush();
             }
-        }catch (IOException e){
-            e.printStackTrace();
-            cerrarComunicacion(socket, bufferedWriter, bufferedReader);
+        } catch (IOException e){
+            cerrarComunicacion();
         }
     }
 
     public void escucharMensaje(){
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                String mensajeDesdeChat;
-                while (socket.isConnected()){
-                    try {
-                        mensajeDesdeChat=bufferedReader.readLine();
-                        System.out.println(mensajeDesdeChat);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        cerrarComunicacion(socket, bufferedWriter, bufferedReader);
-                    }
+        new Thread(() -> {
+            String mensaje;
+            try {
+                while ((mensaje = bufferedReader.readLine()) != null){
+                    System.out.println(mensaje);
                 }
+            } catch (IOException e){
+                cerrarComunicacion();
             }
         }).start();
     }
 
     public static void main(String[] args) throws IOException{
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Introduce tu nombre");
-        String nombreCliente = scanner.next();
+        System.out.print("Introduce tu nombre: ");
+        String nombreCliente = scanner.nextLine();
 
-        Socket socket = new Socket("localhost", 6969);
-        Cliente cliente = new Cliente(socket,nombreCliente);
+        Socket socket = new Socket("localhost", 5000);
+
+        Cliente cliente = new Cliente(socket, nombreCliente);
         cliente.escucharMensaje();
         cliente.enviarMensaje();
     }
-
-
 }
